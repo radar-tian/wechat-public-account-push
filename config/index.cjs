5dc11f4db66ca8a5bf18818bd555e8fb50d967fb
radar-tian <116779735+radar-tian@users.noreply.github.com>
2022-12-01 13:42:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -53,8 +53,6 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      PROVINCE: '陕西',
-      CITY: '西安',
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'old6d5_zqRusHPt1AqFRKEAEiNvg',
